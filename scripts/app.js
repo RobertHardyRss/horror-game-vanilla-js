88ce53090439c15183aa17e0d61ed79d2e75904e
Robert Hardy <rhardy@dcsdk12.org>
2022-01-26 16:24:33 -0700

    Created player class

--- scripts/app.js
+++ scripts/app.js
@@ -5,3 +5,43 @@ const canvas = document.getElementById("game-canvas");
 const ctx = canvas.getContext("2d");
 canvas.width = 800;
 canvas.height = 600;
+
+class Player {
+	constructor() {
+		this.width = 32;
+		this.height = 32;
+		this.x = canvas.width / 2 - this.width / 2;
+		this.y = canvas.height / 2 - this.height / 2;
+
+		this.wireUpEvents();
+	}
+
+	wireUpEvents() {
+		window.addEventListener("keydown", (e) => {
+			console.log(e.key);
+		});
+	}
+
+	update() {}
+
+	render() {
+		ctx.save();
+		ctx.fillStyle = "green";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.restore();
+	}
+}
+
+let player = new Player();
+
+function gameLoop() {
+	// clear off the canvas
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	player.update();
+	player.render();
+
+	requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
