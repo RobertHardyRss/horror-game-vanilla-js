3bd61cca2e48dea84ff24d8fc3b89e5778c8cb03
Robert Hardy <rhardy@dcsdk12.org>
2022-01-26 15:33:04 -0700

    Setup

---
+++ scripts/app.js
@@ -0,0 +1,7 @@
+//@ts-check
+/** @type {HTMLCanvasElement} */
+//@ts-ignore
+const canvas = document.getElementById("game-canvas");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 600;
