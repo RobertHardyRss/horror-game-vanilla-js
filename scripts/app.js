517fbe95a8ebb426edb70e08901d7e9b2a17aed3
Robert Hardy <rhardy@dcsdk12.org>
2022-01-27 17:35:36 -0700

    Implement player movement

--- scripts/app.js
+++ scripts/app.js
@@ -13,16 +13,84 @@ class Player {
 		this.x = canvas.width / 2 - this.width / 2;
 		this.y = canvas.height / 2 - this.height / 2;
 
+		this.isMovingUp = false;
+		this.isMovingDown = false;
+		this.isMovingRight = false;
+		this.isMovingLeft = false;
+		this.isSneaking = false;
+		this.isRunning = false;
+
+		this.baseSpeed = 10;
+
 		this.wireUpEvents();
 	}
 
 	wireUpEvents() {
 		window.addEventListener("keydown", (e) => {
-			console.log(e.key);
+			// console.log(e.key);
+			this.toggleMovement(e.key, true);
+		});
+
+		window.addEventListener("keyup", (e) => {
+			// console.log(e.key);
+			this.toggleMovement(e.key, false);
 		});
 	}
 
-	update() {}
+	toggleMovement(key, toggleValue) {
+		switch (key) {
+			case "ArrowUp":
+			case "w":
+				this.isMovingUp = toggleValue;
+				break;
+			case "ArrowDown":
+			case "s":
+				this.isMovingDown = toggleValue;
+				break;
+			case "ArrowRight":
+			case "d":
+				this.isMovingRight = toggleValue;
+				break;
+			case "ArrowLeft":
+			case "a":
+				this.isMovingLeft = toggleValue;
+				break;
+			case "Control":
+				this.isSneaking = toggleValue;
+				break;
+			case "Shift":
+				this.isRunning = toggleValue;
+				break;
+		}
+	}
+
+	update() {
+		let speedMultiplier = 1;
+
+		if (this.isRunning && !this.isSneaking) {
+			speedMultiplier = 2;
+		} else if (this.isSneaking && !this.isRunning) {
+			speedMultiplier = 0.5;
+		}
+
+		let speed = this.baseSpeed * speedMultiplier;
+
+		if (this.isMovingUp) {
+			this.y -= speed;
+		}
+
+		if (this.isMovingDown) {
+			this.y += speed;
+		}
+
+		if (this.isMovingRight) {
+			this.x += speed;
+		}
+
+		if (this.isMovingLeft) {
+			this.x -= speed;
+		}
+	}
 
 	render() {
 		ctx.save();
