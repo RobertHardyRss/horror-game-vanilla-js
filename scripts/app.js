aed9dbfea97deca863a47bec20b581d608e0e489
Robert Hardy <rhardy@dcsdk12.org>
2022-01-31 16:24:00 -0700

    Add monster class and time elapsed

--- scripts/app.js
+++ scripts/app.js
@@ -20,7 +20,7 @@ class Player {
 		this.isSneaking = false;
 		this.isRunning = false;
 
-		this.baseSpeed = 10;
+		this.baseSpeed = 3;
 
 		this.wireUpEvents();
 	}
@@ -64,7 +64,7 @@ class Player {
 		}
 	}
 
-	update() {
+	update(elapsedTime) {
 		let speedMultiplier = 1;
 
 		if (this.isRunning && !this.isSneaking) {
@@ -100,14 +100,45 @@ class Player {
 	}
 }
 
-let player = new Player();
+class Monster {
+	constructor() {
+		this.width = 32;
+		this.height = 32;
+		this.x = 0;
+		this.y = 0;
 
-function gameLoop() {
+		this.baseSpeed = 3;
+	}
+
+	update(elapsedTime) {}
+
+	render() {
+		ctx.save();
+		ctx.fillStyle = "red";
+		ctx.fillRect(this.x, this.y, this.width, this.height);
+		ctx.restore();
+	}
+}
+
+let player = new Player();
+let m1 = new Monster();
+
+let gameObjects = [player, m1];
+
+let currentTime = 0;
+
+function gameLoop(timestamp) {
+	//console.log(timestamp);
 	// clear off the canvas
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-	player.update();
-	player.render();
+	let elapsedTime = Math.floor(timestamp - currentTime);
+	currentTime = timestamp;
+
+	gameObjects.forEach((o) => {
+		o.update(elapsedTime);
+		o.render();
+	});
 
 	requestAnimationFrame(gameLoop);
 }
